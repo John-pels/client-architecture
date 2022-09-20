e7b99223aaa93079c22cd9f9300a62ca792905f9
John Oluwaseyi Ajeigbe <johhn_pels@Johns-MacBook-Pro.local>
2022-09-20 14:19:00 +0100

    feat: add card component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['i.pravatar.cc'],
+  },
 };
 
 module.exports = nextConfig;
