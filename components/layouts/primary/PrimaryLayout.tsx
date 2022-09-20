d0bb9ce35dc580cfe5fde18154fea0f1d4252aa4
John Oluwaseyi Ajeigbe <johhn_pels@Johns-MacBook-Pro.local>
2022-09-20 14:49:09 +0100

    feat: add page layouts

---
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import React from 'react';
+import styles from './PrimaryLayout.module.css';
+
+export interface IPrimaryLayout { children?: React.ReactNode }
+
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+    return (
+        <>
+            <Head>
+                <title>Primary Layout Example</title>
+            </Head>
+            <main className={styles.main}>{children}</main>
+        </>
+    );
+};
+
+export default PrimaryLayout;
